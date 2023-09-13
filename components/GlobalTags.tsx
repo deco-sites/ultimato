c6141abc2e8ea2ca876315973954e05f29d5cdd7
Gustavo Rocha <gustavo@onserp.com.br>
2023-09-13 15:17:15 -0300

    use google fonts

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -29,108 +29,16 @@ function GlobalTags() {
       <link rel="manifest" href={asset("/site.webmanifest")} />
 
       {/* Fonts */}
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat Bold'), 
-                local('Montserrat-Bold'),
-                url(${asset("/fonts/Montserrat-Bold.woff2")}) format('woff2'),
-              font-weight: bold;
-              font-style: normal;
-              font-display: swap;
-            }
-            
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat ExtraLight'), 
-                local('Montserrat-ExtraLight'),
-                url(${
-            asset("/fonts/Montserrat-ExtraLight.woff2")
-          }) format('woff2'),
-              font-weight: 200;
-              font-style: normal;
-              font-display: swap;
-            }
-            
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat ExtraBold'), 
-                local('Montserrat-ExtraBold'),
-                url(${
-            asset("/fonts/Montserrat-ExtraBold.woff2")
-          }) format('woff2'),
-              font-weight: 800;
-              font-style: normal;
-              font-display: swap;
-            }
-            
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat SemiBold'), 
-                local('Montserrat-SemiBold'),
-                url(${
-            asset("/fonts/Montserrat-SemiBold.woff2")
-          }) format('woff2'),
-              font-weight: 600;
-              font-style: normal;
-              font-display: swap;
-            }
-            
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat Regular'), 
-                local('Montserrat-Regular'),
-                url(${
-            asset("/fonts/Montserrat-Regular.woff2")
-          }) format('woff2'),
-              font-weight: normal;
-              font-style: normal;
-              font-display: swap;
-            }
-            
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat Medium'), 
-                local('Montserrat-Medium'),
-                url(${asset("/fonts/Montserrat-Medium.woff2")}) format('woff2'),
-              font-weight: 500;
-              font-style: normal;
-              font-display: swap;
-            }
-            
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat Light'), 
-                local('Montserrat-Light'),
-                url(${asset("/fonts/Montserrat-Light.woff2")}) format('woff2'),
-              font-weight: 300;
-              font-style: normal;
-              font-display: swap;
-            }
-            
-            @font-face {
-              font-family: 'Montserrat';
-              src:
-                local('Montserrat Thin'), 
-                local('Montserrat-Thin'),
-                url(${asset("/fonts/Montserrat-Thin.woff2")}) format('woff2'),
-              font-weight: 100;
-              font-style: normal;
-              font-display: swap;
-            }
-            `,
-        }}
-      >
-      </style>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,700;1,300&family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 }
